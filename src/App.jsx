d5404260121af4066ce35fd434a2e7ea56528d17
denishaiih <denisha.iihglobal@gmail.com>
2024-11-17 11:38:29 +0530

    issue fuxed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DragDrop from './DragDrop';
 const App = () => {
     return (
         <div className=" flex flex-col h-screen px-2 md:px-20 text-center space-y-8 overflow-y-auto scrollbar-hide ">
-            <DragDrop />
+        <DragDrop />
         </div>
     );
 };
